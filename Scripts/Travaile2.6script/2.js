abdcf7bacf03ac0130f9f189b543c29f3efe4c4b
TristanAb <90333891+TristanAb@users.noreply.github.com>
2021-10-04 15:04:49 -0400

    Update 2.js

--- Scripts/Travaile2.6script/2.js
+++ Scripts/Travaile2.6script/2.js
@@ -1,9 +1,19 @@
-
-let prenom = prompt(  "Quel est votre prenom");
-let nomDuMilieu = prompt( "Quelle est votre milleux nom");
-let nomDeFamille = prompt(  "Quel est votre nom de famille");
- 
-alet(  "Initale:" + prenom.charAt(0) + "." + nomDuMilieu.charAt(0) + "." + nomDeFamille.charAt(0) + ".");
-alert(  "Nom complet: " + prenom + " " + nomDuMilieu + " " +nomDeFamille)
-var username = prenom.charAt(0) + nomDuMilieu.charAt(0) + nomDeFamille;
-alert("Username: " + username.toLowerCase())
+function Finitial(){
+  let prenom = document.getElementById("txtPrenom").value;
+let nomDuMilieu = document.getElementById("txtNomMilieux").value;
+let nomDeFamille = document.getElementById("txtNomFamille").value;
+  alert(  "Initale:" + prenom.charAt(0) + "." + nomDuMilieu.charAt(0) + "." + nomDeFamille.charAt(0) + ".");
+}
+function Fcomplet(){
+  let prenom = document.getElementById("txtPrenom").value;
+let nomDuMilieu = document.getElementById("txtNomMilieux").value;
+let nomDeFamille = document.getElementById("txtNomFamille").value;
+  alert(  "Nom complet: " + prenom + " " + nomDuMilieu + " " +nomDeFamille);
+}
+function Futilisateur(){
+  let prenom = document.getElementById("txtPrenom").value;
+let nomDuMilieu = document.getElementById("txtNomMilieux").value;
+let nomDeFamille = document.getElementById("txtNomFamille").value;
+  var username = prenom.charAt(0) + nomDuMilieu.charAt(0) + nomDeFamille;
+  alert("Username: " + username.toLowerCase());
+}
