5ff5ddbc0353252e5aaeb3d73655a861ac1673e5
TristanAb <90333891+TristanAb@users.noreply.github.com>
2021-10-04 14:26:30 -0400

    Update 2.js

--- Scripts/Travaile2.6script/2.js
+++ Scripts/Travaile2.6script/2.js
@@ -1,1 +1,9 @@
 
+let prenom = prompt(  "Quel est votre prenom");
+let nomDuMilieu = prompt( "Quelle est votre milleux nom");
+let nomDeFamille = prompt(  "Quel est votre nom de famille");
+ 
+alet(  "Initale:" + prenom.charAt(0) + "." + nomDuMilieu.charAt(0) + "." + nomDeFamille.charAt(0) + ".");
+alert(  "Nom complet: " + prenom + " " + nomDuMilieu + " " +nomDeFamille)
+var username = prenom.charAt(0) + nomDuMilieu.charAt(0) + nomDeFamille;
+alert("Username: " + username.toLowerCase())
