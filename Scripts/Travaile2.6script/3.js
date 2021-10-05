515fc4badc46ea82b4db9ef5bfdf3cf2b0960c92
TristanAb <90333891+TristanAb@users.noreply.github.com>
2021-10-05 11:52:00 -0400

    Update 3.js

--- Scripts/Travaile2.6script/3.js
+++ Scripts/Travaile2.6script/3.js
@@ -1,1 +1,29 @@
+function chkchk (){
+  var table = [0,0,0,0]
+  table[0] = document.getElementById("chkProgramation1").checked;
+  table[1] = document.getElementById("chkProgramation2").checked;
+  table[2] = document.getElementById("chkProgramation3").checked;
+  table[3] = document.getElementById("chkProgramation4").checked;
+  var s = document.getElementById("radio").checked;
+  if (s == true){
+    console.log("Question 1:oui")
+    
+  } else{
+    console.log("Question 1:non")
+  }
+  if (table[3] ==  true ){
+    console.log("Question 2:non")
+  } else if (table [0] == false ){
+    console.log("Question 2:non")
+    
+  } else if (table [1] ==false ){
+    console.log("Question 2:non")
+    
+  } else if (table [2] == false){
+    console.log("Question 2:non")
+    
+  } else {
+    console.log("Question 2:oui")
+  }
 
+}
